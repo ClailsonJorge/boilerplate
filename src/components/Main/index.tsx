6249a50e042a0da29f1c32cf33536390cfce1892
ClailsonJorge <clailsonjorge@gmail.com>
2021-05-16 13:26:56 -0300

    feat: boilerplate to Main page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,23 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>react avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+
+    <S.Title>React Avançado - Boilerplate</S.Title>
+
+    <S.Description>
+      Typescript, ReactJs, NextJs e Styled Components
+    </S.Description>
+
+    <S.ImgMain
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor sentado em frente a telas de códigos."
+    />
+  </S.Wrapper>
 )
 
 export default Main
